9a9f30c2ed39f346f768c86d1d3c09a4127cbf5c
ingeborgvpt <ingeborgvpt@gmail.com>
2026-09-24 10:44:51 +0200

    Assignment fixed

--- js/script.js
+++ js/script.js
@@ -17,11 +17,17 @@ const getMoreInfo = document.getElementById("moreInfo");
 
 // Eksempel: vi lytter efter klik på knappen og kører en anonym function
 getToggleBtn.addEventListener("click", function() {
-
+    if (getMoreInfo.hidden == true) {
+        getMoreInfo.hidden = false;
+        getToggleText.textContent = `Vis mindre`;
+        getToggleIcon.style.transform = `rotate(180deg)`;
+    } else {
+     getMoreInfo.hidden = true
+    getToggleText.textContent = `Vi mere`;
+    getToggleIcon.style.transform = `rotate(0deg)`; }
+}
     // Skriv if/else-strukturen selv herinde.
-    //
     // Nyt i dag: getMoreInfo.hidden viser/skjuler et element med det samme (true = skjult, false = synligt).
-    //
     // Hvis getMoreInfo.hidden er true (dvs. indholdet er skjult lige nu), så:
     //   - sæt getMoreInfo.hidden til false
     //   - sæt getToggleText.textContent til `Vis mindre`
@@ -30,8 +36,9 @@ getToggleBtn.addEventListener("click", function() {
     //   - sæt getMoreInfo.hidden til true
     //   - sæt getToggleText.textContent til `Vis mere`
     //   - sæt getToggleIcon.style.transform til `rotate(0deg)`
-    //
     // Ekstra (valgfrit, hvis du er hurtigt færdig): brug getToggleBtn.setAttribute("aria-expanded", "true"/"false")
     // i hver gren, så skærmlæsere også kan se, om indholdet er foldet ud.
 
-});
+);
+
+
